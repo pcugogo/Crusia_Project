7e10a1f6d42fd08091a1ff471758973190933b21
hhyoungsu <hhyoungsu@gmail.com>
2017-08-13 04:15:37 +0900

    Update Crusia
    - 유저 정보 패치 작동 완료

--- Crusia/Crusia/CurrentUserInfoService.swift
+++ Crusia/Crusia/CurrentUserInfoService.swift
@@ -52,34 +52,38 @@ class CurrentUserInfoService {
     
     func patchUserInfo() {
         
-        
-//        print("tttttttttttttttttttttttttttttttttttttttttttttttttttttt",header)
-//        let parameters:Parameters = HostingService.shared.houseParameters()
-//        let httpHeader:HTTPHeaders = ["Authorization":"Token \(header)"]
-//        print("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh", httpHeader)
-//        Alamofire.request("http://crusia.xyz/apis/house/", method: .post, parameters: parameters,  headers: httpHeader).validate().responseJSON { response in
-//            switch response.result {
-//                
-//            case .success(let value):
-//                
-//                print("Validation Successful")
-//                
-//                
-//            case .failure(let error):
-//                print(error)
-//                
-//            }
-//            
-//        }
-        
         let token: String = UserDefaults.standard.object(forKey: "token") as! String
-        let httpHeader: HTTPHeaders = ["Authorization": "Token \(token)"]
+        let httpHeader: HTTPHeaders = ["Authorization": "Token " + token]
 //        let parameters: Parameters = ["email": emailAddress, "password1": password, "password2": confirm]
+        
+        print("PatchUserInfo..........................")
+        print(httpHeader)
+        
+//        let userName = CurrentUserInfoService.shared.currentUser?.userName.stringValue
+//        let firstName = CurrentUserInfoService.shared.currentUser?.firstName.stringValue
+//        let lastName = CurrentUserInfoService.shared.currentUser?.lastName.stringValue
+//        let gender = CurrentUserInfoService.shared.currentUser?.gender.stringValue
+//        let phoneNumber = CurrentUserInfoService.shared.currentUser?.phoneNum.stringValue
+//        let birthDay = CurrentUserInfoService.shared.currentUser?.birthday.stringValue
+//        let preferLanguage = CurrentUserInfoService.shared.currentUser?.prefLanguage.stringValue
+//        let livingSite = CurrentUserInfoService.shared.currentUser?.livingSite.stringValue
+        
+        
+//        let parameters: Parameters = ["username": userName!,
+//                                      "first_name": firstName!,
+//                                      "last_name": lastName!,
+//                                      "gender": gender!,
+//                                      "phone_num": phoneNumber!,
+//                                      "birthday": birthDay!,
+//                                      "preference_language": preferLanguage!,
+//                                      "living_site": livingSite!]
+        
         let parameters: Parameters = (CurrentUserInfoService.shared.currentUser?.dic)!
+
+        let currentUserPk: Int = UserDefaults.standard.object(forKey: "userPk") as! Int
         
-        let currentUserPk = UserDefaults.standard.object(forKey: "userPk") as! Int
         
-        Alamofire.request("http://crusia.xyz/apis/user/\(currentUserPk)", method: .patch, parameters: parameters, headers: httpHeader).validate().responseJSON { response in
+        Alamofire.request("http://crusia.xyz/apis/user/\(currentUserPk)/", method: .patch, parameters: parameters, headers: httpHeader).validate().responseJSON { (response) in
             
             switch response.result {
                 
@@ -92,11 +96,16 @@ class CurrentUserInfoService {
                 
                 self.currentUser = User.init(user: json)
                 
+                CurrentUserInfoService.shared.setCurrentUser()
+
+                
             case .failure(let error):
                 print(error)
                 
             }
+            
         }
+        
     }
     
     func edit() {
